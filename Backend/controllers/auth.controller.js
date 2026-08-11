836211deaed4f57bca4b68c93fe1498f0bb22693
vaishu-1221 <vaishnavirajput028@gmail.com>
2026-08-11 13:10:53 +0530

    Update cookie settings for security enhancements

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -70,8 +70,8 @@ const signIn=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:10*365*24*60*60*1000,
-            secure:false,
-            sameSite:"Strict"
+            secure:true,
+            sameSite:"none"
         })
         return res.status(201).json(user)
     } catch (error) {
