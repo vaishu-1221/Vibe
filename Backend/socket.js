f5beb808e0885174501055ab00332549ab6bb672
vaishu-1221 <vaishnavirajput028@gmail.com>
2026-08-11 13:12:32 +0530

    Update CORS origin for socket server

--- Backend/socket.js
+++ Backend/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://vibe-frontend-u02o.onrender.com",
         methods: ["GET", "POST"]
     }
 })
